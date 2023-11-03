575e4382d7ffc4f0b495dc6e3d22feab48ccdeea
RatBaugette <pleasepeople123@gmail.com>
2023-11-03 15:17:40 +0800

    Created Test lab2.py and maded changes on lab2 exercise 6

---
+++ Test_Lab2.py
@@ -0,0 +1,19 @@
+from lab2.Exercise6 import exercise6
+
+def test_calc_median_temperature():
+    expected_result = 3
+    arr = [1,2,3,4,5]
+    result = exercise6.calc_median_temperature(arr)
+    assert (expected_result == result)
+
+def test_clac_average_temperature():z
+    expected_result = 2
+    arr = [1,2,3]
+    result = exercise6.calc_average_temperature(arr)
+    assert (expected_result == result)
+
+def test_min_max_temperature():
+    expected_result = [1,3]
+    arr = [1,2,3]
+    result = exercise6.calc_min_max_temperature(arr)
+    assert (expected_result==result)
--- lab2/Exercise6/exercise6.py
+++ lab2/Exercise6/exercise6.py
@@ -1,25 +1,26 @@
-def calc_average_temperature():
-    Temperature = get_values()
-    print("Average temperature:", sum(Temperature)/len(Temperature))
-def calc_min_max_temperature():
-    Temperature = get_values()
+def calc_average_temperature(Temperature):
+    average = sum(Temperature) / len(Temperature)
+    print("Average temperature:", average)
+    return average
+
+
+def calc_min_max_temperature(Temperature):
     min_temp = min(Temperature)
     max_temp = max(Temperature)
     print("The min Temperature is ", min_temp, "\nThe max Temperature is ", max_temp)
-def calc_median_temperature():
-    Temperature = get_values()
+    return [min_temp, max_temp]
+
+
+def calc_median_temperature(Temperature):
     Temperature.sort()
     length_of_Temp = len(Temperature)
     if length_of_Temp % 2 == 1:
-        median = Temperature[length_of_Temp//2]
+        median = Temperature[length_of_Temp // 2]
     else:
-        median = (Temperature[length_of_Temp//2 - 1] + Temperature[length_of_Temp// 2])/2
+        median = (Temperature[length_of_Temp // 2 - 1] + Temperature[length_of_Temp // 2]) / 2
 
     print("The median is ", median)
-
-
-
-
+    return median
 
 
 def get_values():
@@ -29,12 +30,3 @@ def get_values():
     return Input
 
 
-Choice = input("Enter 1 for average \nEnter 2 for min and max\nEnter 3 for median temperature\n")
-if (Choice == "1"):
-    calc_average_temperature()
-
-if (Choice == "2"):
-    calc_min_max_temperature()
-
-if (Choice == "3"):
-    calc_median_temperature()
